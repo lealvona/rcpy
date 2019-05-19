88a9a5c24b8118861e5d41e887233345b2bef72b
Leal Vona <lealvona.work@gmail.com>
2019-05-19 06:22:50 -0400

    changes to make it work

--- examples/rcpy_dsm.py
+++ examples/rcpy_dsm.py
@@ -10,14 +10,14 @@ def dsm_test():
     # Read DSM channels via function calls.
     for ch in range(1, dsm.CHANNEL_COUNT+1):
         raw = dsm.get_ch_raw(ch)
-        normal = dsm.get_ch_normalized(ch)
-        print("channel={} : raw={:4} normal={:+6.2f}".format(ch, raw, normal))
+        # normal = dsm.get_ch_normalized(ch)
+        print("channel={} : raw={:4} normal={:+6.2f}".format(ch, raw))
 
     # Read DSM channels via class instances.
     for ch, a in enumerate(dsm.dsm):
         raw = a.get_ch_raw()
-        normal = a.get_ch_normalized()
-        print("dsm[{}] : raw={:4} normal={:+6.2f}".format(ch, raw, normal))
+        # normal = a.get_ch_normalized()
+        print("dsm[{}] : raw={:4} normal={:+6.2f}".format(ch, raw))
 
     # Read DC Jack and Battery voltages via function calls.
     # dc_jack_voltage = dsm.get_dc_jack_voltage()
@@ -32,5 +32,6 @@ def dsm_test():
     # print("battery : voltage={:+6.2f}".format(battery_voltage))
         
 if __name__ == "__main__":
-    dsm_test()
+    while true:
+        dsm_test()
 
